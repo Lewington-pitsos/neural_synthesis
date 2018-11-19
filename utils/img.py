8be5af0b2602bb9fe2439f1f148ccd02b8a3dd36
lewington <lewington@student.unimelb.edu.au>
2018-11-19 15:48:48 +1100

    added working pyramid code

---
+++ utils/img.py
@@ -0,0 +1,22 @@
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+from matplotlib import pyplot as plt
+
+
+def image_loader(image_path, crop_size=300):
+    transform = transforms.Compose([
+        transforms.RandomCrop(crop_size),
+        transforms.ToTensor(),
+    ])
+
+
+    """load image, returns cuda tensor"""
+    image = Image.open(image_path)
+    image = transform(image).float()
+    image = image.unsqueeze(0)  #this is for VGG, may not be needed for ResNet
+    return image.cuda()  #assumes that you're using GPU
+
+def show_image(image_tensor):
+    np_image = image_tensor.squeeze().cpu().detach().numpy()
+    plt.imshow(np.transpose(np_image, (1, 2, 0)))
