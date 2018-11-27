ab2a6f28f0b16f7f06a2b2db4411f917191823ca
lewington <lewington@student.unimelb.edu.au>
2018-11-28 10:59:43 +1100

    smoothness loss correected

--- utils/losses.py
+++ utils/losses.py
@@ -2,11 +2,12 @@ import torch
 import numpy as np
 from utils import stats
 
-def smoothing_loss(tensor):
+def smoothing_loss(tensor, sigma=0.0001):
     """
     Calculates smoothing factor loss according to "Deep Correlations for Texture synthesis"
     """
     # print(tensor.size()) # [M, Q]
+    print("tensot rum: {}".format(torch.sum(tensor)))
     axis_offset = tensor.dim() - 2
     
     shifts = stats.all_shifts(tensor, axis_offset)
@@ -17,14 +18,22 @@ def smoothing_loss(tensor):
     
     diff_sq = differences ** 2
     # print(diff_sq.size()) # should be [8, M, Q]
+    print(torch.sum(diff_sq))
+
     
-    diff_sq_std = diff_sq * (-tensor.std())
+    diff_sq_std = diff_sq * (-sigma)
     # print(diff_sq_std.size()) # should be [8, M, Q]
+    print(torch.sum(diff_sq_std))
     
     sum_exp = torch.sum(torch.exp(diff_sq_std), dim=0)   
     # print(sum_exp.size()) # should be [M, Q]
+    print(torch.sum(sum_exp))
+    print(min(sum_exp))
     
     sum_log = torch.sum(torch.log(sum_exp))
+    #sum_log = torch.sum(sum_exp)
+
     # print(sum_log.size()) # should be []
-    
-    return 1 / (sum_log * (2 * tensor.std()))
+
+    print(sum_log)
+    return (1) / ((sum_log * (2 * sigma)))
